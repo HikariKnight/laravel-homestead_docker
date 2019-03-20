572771079768817cb64af7dab6b5eff7f82c6103
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2019-03-20 17:30:53 +0100

    added .gitignore and readme and updated build-image.sh to reflect the docker group change.

--- build-image.sh
+++ build-image.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 DIR=$(dirname $0)
 REALDIR=$(readlink -f $DIR)
+DGID=$(sed -nr "s/^docker:x:([0-9]+):.*/\1/p" /etc/group)
 docker build -t laravel-homestead $DIR
 printf "
 You can now make a laravel container using the below command and access the site at http://127.0.0.1:8000
-\tdocker run -dit --name laravel-dev -p 8000-8010:8000-8010 -v $REALDIR/html:/www --user $(id -u):$(id -g) laravel-homestead
+\tdocker run -dit --rm --name laravel-dev -p 8000-8010:8000-8010 -v "$REALDIR/html":/www --user 0:$DGID laravel-homestead
 
 \tNOTE: you can pass a project name at the end of the command to use as a 
 \tdefault laravel project each time it is started in the future (default: site)
